8066c69abab03a8e0e0f55527dd8212e2b83a77e
yosipy <todacchi.desu@gmail.com>
2024-03-12 19:51:14 +0900

    Top page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./index.css"
 import App from "./App"
+import { TopPage } from "@/pages/TopPage/TopPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <TopPage />,
   },
   {
     path: "/app",
