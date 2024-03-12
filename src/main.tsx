f4eecd719951389e86bd9e0f3bcca38b79c8fdb4
yosipy <todacchi.desu@gmail.com>
2024-03-12 19:43:35 +0900

    Render App

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./index.css"
+import App from "./App"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+  },
+  {
+    path: "/app",
+    element: <App />,
   },
 ])
 
